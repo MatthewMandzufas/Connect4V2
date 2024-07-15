bc9e3e9e102a6788935c38f7a7cfa29c503e39a3
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-07-15 14:11:43 +0800

    Added container structure

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.post("/login", (req, res) => {});
+
+app.post("/logout", (req, res) => {});
+
+app.post("/signup", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
