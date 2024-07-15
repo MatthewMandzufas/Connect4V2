3aee8d93fd7229ca228b6da9579981146ca6f44c
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-07-15 16:33:32 +0800

    RED: in-memory-user-repository › given the details for a user who does not exist › creates a user

---
+++ src/user/in-memory-user-repository.spec.ts
@@ -0,0 +1,22 @@
+import InMemoryUserRepository from "@/user/in-memory-user-repository";
+
+describe("in-memory-user-repository", () => {
+  describe("given the details for a user who does not exist", () => {
+    it("creates a user", () => {
+      const inMemoryUserRepository = new InMemoryUserRepository();
+      const createdUser = inMemoryUserRepository.create({
+        firstName: "John",
+        lastName: "Doe",
+        email: "john.doe@email.com",
+      });
+      expect(createdUser).toEqual(
+        expect.objectContaining({
+          firstName: "John",
+          lastName: "Doe",
+          email: "john.doe@email.com",
+          uuid: expect.toBeUUID(),
+        })
+      );
+    });
+  });
+});
