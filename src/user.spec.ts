4367800d9dd235d43669d8f08ae6f0664097f2fb
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-07-16 16:22:51 +0800

    user-integration › sign-up › given invalid user details › forbids the creation of the user

--- src/user.spec.ts
+++ src/user.spec.ts
@@ -27,11 +27,11 @@ describe("user-integration", () => {
       });
     });
     describe("given a user already exists with a given email", () => {
-      it("forbids the creation of a user the same email", async () => {
+      it("forbids the creation of a user with the same email", async () => {
         const johnDoeUser = {
-          firstName: "John",
-          lastName: "Doe",
-          email: "john.doe@email.com",
+          firstName: "Alex",
+          lastName: "Bon",
+          email: "alex.don@email.com",
         };
         await request(app).post("/user/signup").send(johnDoeUser);
 
@@ -42,6 +42,25 @@ describe("user-integration", () => {
         expect(response.body).toEqual({
           errors: ["A user with that email already exists"],
         });
+        expect(response.headers["content-type"]).toMatch(/json/);
+      });
+    });
+    describe("given invalid user details", () => {
+      it("forbids the creation of the user", async () => {
+        const userWithMissingPassword = {
+          firstName: "D",
+          lastName: "A",
+          email: "d.a@email.com",
+        };
+
+        const response = await request(app)
+          .post("/user/signup")
+          .send(userWithMissingPassword);
+
+        expect(response.body.errors).toEqual([
+          "Invalid user details provided. Expected a first name, last name, email and password.",
+        ]);
+        expect(response.statusCode).toBe(403);
         expect(response.headers["content-type"]).toMatch(/json/);
       });
     });
