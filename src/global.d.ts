cabaf76b349b3087b66ccbf8b193d7dcc4a57bdf
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-07-16 14:58:13 +0800

    Fixes toBeUUID typescript warnings

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,24 +1,3 @@
-interface CustomMatcher<R = jest.CustomMatcherResult> {
-  toBeUuid(): R;
-}
-declare module "expect" {
-  interface MatcherFunction<R> {
-    toBeUUID(received: string): R;
-  }
-  interface AsymmetricMatchers {
-    toBeUUID(received: string): void;
-  }
-  interface Matchers<R> {
-    toBeUUID(received: string): R;
-  }
-}
-
-// declare namespace jest {
-//   interface Matchers<R> {
-//     toBeUUID(): R;
-//   }
-// }
-
 export type Uuid = `${string}-${string}-${string}-${string}-${string}`;
 
 export type Env = "production" | "test";
