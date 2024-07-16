eef8919afc304059c04b90cc68803ef14aa529c8
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-07-16 15:11:14 +0800

    RED: user-integration › sign-up › given a user already exists with a given email › forbids the creation of a user the same email

--- src/user.spec.ts
+++ src/user.spec.ts
@@ -5,11 +5,15 @@ describe("user-integration", () => {
   describe("sign-up", () => {
     describe("given a user does not yet exist", () => {
       it("creates a user ", async () => {
-        const response = await request(app).post("/user/signup").send({
+        const johnDoeUser = {
           firstName: "John",
           lastName: "Doe",
           email: "john.doe@email.com",
-        });
+        };
+
+        const response = await request(app)
+          .post("/user/signup")
+          .send(johnDoeUser);
         expect(response.statusCode).toBe(201);
         expect(response.body).toEqual(
           expect.objectContaining({
@@ -19,6 +23,25 @@ describe("user-integration", () => {
             uuid: expect.toBeUUID(),
           })
         );
+        expect(response.headers["content-type"]).toMatch(/json/);
+      });
+    });
+    describe("given a user already exists with a given email", () => {
+      it("forbids the creation of a user the same email", async () => {
+        const johnDoeUser = {
+          firstName: "John",
+          lastName: "Doe",
+          email: "john.doe@email.com",
+        };
+        await request(app).post("/user/signup").send(johnDoeUser);
+
+        const response = await request(app)
+          .post("/user/signup")
+          .send(johnDoeUser);
+        expect(response.statusCode).toBe(403);
+        expect(response.body).toEqual({
+          errors: ["A user with that email already exists"],
+        });
         expect(response.headers["content-type"]).toMatch(/json/);
       });
     });
