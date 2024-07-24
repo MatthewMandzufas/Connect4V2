a6342010af54f2c03b5923eadfeefb21573eb437
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-07-24 17:02:52 +0800

    GREEN: get-is-user-authorized > given a token > which was not issued for the user > returns false

--- src/get-is-user-authorized.ts
+++ src/get-is-user-authorized.ts
@@ -1,10 +1,15 @@
-import { jwtDecrypt, jwtVerify, KeyLike } from "jose";
+import { jwtDecrypt, KeyLike } from "jose";
 import { JWEInvalid, JWTExpired } from "jose/errors";
 
-const getIsUserAuthorized = async (token: string, privateKey: KeyLike) => {
+const getIsUserAuthorized = async (
+  token: string,
+  privateKey: KeyLike,
+  userEmail: string
+) => {
   try {
-    await jwtDecrypt(token, privateKey);
-    await jwtVerify(token, privateKey);
+    const { payload } = await jwtDecrypt(token, privateKey);
+
+    if (payload.userName !== userEmail) return false;
   } catch (error) {
     if (error instanceof JWEInvalid || error instanceof JWTExpired)
       return false;
