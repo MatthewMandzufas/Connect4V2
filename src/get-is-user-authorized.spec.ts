cc75ec2f0ecbba8a9de6de82d6d90a356ce8256f
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-07-24 16:26:14 +0800

    RED: get-is-user-authorized › given a token › which is expired › returns false

--- src/get-is-user-authorized.spec.ts
+++ src/get-is-user-authorized.spec.ts
@@ -1,5 +1,5 @@
 import getIsUserAuthorized from "@/get-is-user-authorized";
-import { generateKeyPair } from "jose";
+import { EncryptJWT, generateKeyPair } from "jose";
 
 describe("get-is-user-authorized", () => {
   describe("given a token", () => {
@@ -7,6 +7,16 @@ describe("get-is-user-authorized", () => {
       it("returns false", async () => {
         const { privateKey, publicKey } = await generateKeyPair("RS256");
         const token = "pakhjsfdokjahbnsokrdh";
+        expect(getIsUserAuthorized(token, privateKey)).resolves.toBe(false);
+      });
+    });
+    describe("which is expired", () => {
+      it("returns false", async () => {
+        const { privateKey, publicKey } = await generateKeyPair("RS256");
+        const token = await new EncryptJWT()
+          .setProtectedHeader({ alg: "RSA-DAEP-256", enc: "A128CBC-HS256" })
+          .setExpirationTime("1 day ago")
+          .encrypt(publicKey);
         expect(getIsUserAuthorized(token, privateKey)).resolves.toBe(false);
       });
     });
