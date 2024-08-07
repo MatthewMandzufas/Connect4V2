b0838517ece547121f36f219383e496e65b7aa06
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-08-07 15:59:23 +0800

    RED: test-fixture.js > given no parameters > signing up users > signing up a single user > signs up a user

--- src/test-fixture.spec.ts
+++ src/test-fixture.spec.ts
@@ -2,10 +2,49 @@ import TestFixture from "./test-fixture";
 
 describe(`test-fixture.js`, () => {
   describe(`Given no parameters`, () => {
+    const testFixture = new TestFixture();
     it(`returns a text fixture, generating a default app`, () => {
-      const testFixture = new TestFixture();
       expect(testFixture).toBeInstanceOf(TestFixture);
     });
+    describe(`signing up users users`, () => {
+      describe(`signing up a single user`, () => {
+        describe(`given a users details`, () => {
+          it(`signs up a user`, async () => {
+            const userDetails = {
+              firstName: "joe",
+              lastName: "blogs",
+              email: "joe.blogs@email.com",
+              password: "somethingSafe",
+            };
+            const response = await testFixture.signUpUser(userDetails);
+            expect(response.statusCode).toBe(201);
+            expect(response.body).toEqual({
+              firstName: "joe",
+              lastName: "blogs",
+              email: "joe.blogs@email.com",
+              uuid: expect.toBeUUID(),
+            });
+          });
+        });
+      });
+      describe(`signing up multiple users`, () => {
+        describe(`given multiple users details`, () => {
+          it.todo(`signs up all users`);
+        });
+      });
+    });
+    describe(`logging in a user`, () => {
+      describe(`given an existing users details`, () => {
+        it.todo(`logs in the user`);
+      });
+    });
+    describe(`sending an invite`, () => {
+      describe(`given a valid inviter/invitee`, () => {
+        it.todo(`creates an invite`);
+      });
+    });
   });
-  it.todo(`finish test cases for a test fixture`);
+  describe(`Given an app`, () => {
+    it.todo(`returns a text fixture`);
+  });
 });
