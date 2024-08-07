df82df372960090e7e1bcec6e153a6ae1ae2d83a
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-08-07 14:27:02 +0800

    RED: test-fixture.js > given no parameters > returns a test fixture

---
+++ src/test-fixture.spec.ts
@@ -0,0 +1,10 @@
+import TestFixture from "./test-fixture";
+
+describe(`test-fixture.js`, () => {
+  describe(`Given no parameters`, () => {
+    it(`returns a text fixture`, () => {
+      const testFixture = new TestFixture();
+      expect(testFixture).toBe;
+    });
+  });
+});
