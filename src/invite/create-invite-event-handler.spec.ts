1c49f17c77b5c9c7f4edaa373093fa05c0cba897
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-08-14 16:17:55 +0800

    RED: create-invite-event-handler > given an event publisher > creates one event handler for each invite service event

---
+++ src/invite/create-invite-event-handler.spec.ts
@@ -0,0 +1,14 @@
+import createInviteEventHandlers from "./create-invite-event-handler";
+import { InviteEvents } from "./invite-service.d";
+
+describe("create-invite-event-handler", () => {
+  describe("given an event publisher", () => {
+    it("creates one event handler for each invite service event", () => {
+      const mockEventPublisher = jest.fn();
+      const eventHandlers = createInviteEventHandlers(mockEventPublisher);
+      expect(eventHandlers).toEqual({
+        [InviteEvents.INVITATION_CREATED]: expect.any(Function),
+      });
+    });
+  });
+});
