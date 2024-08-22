1254f56cb0ef2c8402728c80d25e42a1bdc49014
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-08-22 14:48:06 +0800

    REFACTOR TEST CASE

--- src/create-dispatch-notification.spec.ts
+++ src/create-dispatch-notification.spec.ts
@@ -53,128 +53,261 @@ afterEach(() => {
 
 describe(`create-dispatch-notification`, () => {
   describe(`given a user connected to a socket`, () => {
-    describe(`when a message is dispatched to the user`, () => {
-      let recipientSocket: Socket;
-      afterEach(() => {
-        recipientSocket.removeAllListeners();
-        recipientSocket.disconnect();
-      });
-      it(`the user receives the message`, async () => {
-        const singleUserPromise = new Promise((resolve) => {
-          resolvePromiseWhenUserJoinsRoom = resolve;
-        });
-        let resolveUserPromise: (value: unknown) => void;
-        const inviteeAuth = await testFixture.signUpAndLoginEmail(
-          "poorguy@email.com"
-        );
-        const token = pipe(split(" "), last)(inviteeAuth);
-
-        recipientSocket = ioc(connectionAddress, {
-          auth: {
-            token,
-          },
-        });
-
-        const userPromise = new Promise((resolve) => {
-          resolveUserPromise = resolve;
-        });
-
-        recipientSocket.connect();
-        recipientSocket.on("example_event", (details) => {
-          resolveUserPromise(details);
+    describe(`and no other users are connected`, () => {
+      describe(`when a message is dispatched to the user`, () => {
+        let recipientSocket: Socket;
+        afterEach(() => {
+          recipientSocket.removeAllListeners();
           recipientSocket.disconnect();
         });
+        it(`the user receives the message`, async () => {
+          const singleUserPromise = new Promise((resolve) => {
+            resolvePromiseWhenUserJoinsRoom = resolve;
+          });
+          let resolveUserPromise: (value: unknown) => void;
+          const inviteeAuth = await testFixture.signUpAndLoginEmail(
+            "poorguy@email.com"
+          );
+          const token = pipe(split(" "), last)(inviteeAuth);
 
-        await singleUserPromise;
-        const dispatchNotification = createDispatchNotification(server);
+          recipientSocket = ioc(connectionAddress, {
+            auth: {
+              token,
+            },
+          });
 
-        dispatchNotification({
-          recipient: "poorguy@email.com",
-          type: "example_event",
-          payload: {
+          const userPromise = new Promise((resolve) => {
+            resolveUserPromise = resolve;
+          });
+
+          recipientSocket.connect();
+          recipientSocket.on("example_event", (details) => {
+            resolveUserPromise(details);
+            recipientSocket.disconnect();
+          });
+
+          await singleUserPromise;
+          const dispatchNotification = createDispatchNotification(server);
+
+          dispatchNotification({
+            recipient: "poorguy@email.com",
+            type: "example_event",
+            payload: {
+              exampleData: "FirstTest!",
+            },
+          });
+
+          return expect(userPromise).resolves.toEqual({
             exampleData: "FirstTest!",
-          },
+          });
         });
+      });
+      describe("when multiple messages are dispatched to the user", () => {
+        let recipientSocket: Socket;
+        afterEach(() => {
+          recipientSocket.removeAllListeners();
+          recipientSocket.disconnect();
+        });
+        it("the user receives all messages", async () => {
+          const singleUserPromise = new Promise((resolve) => {
+            resolvePromiseWhenUserJoinsRoom = resolve;
+          });
+          let resolveUserReceivesMessagesPromise: (value: unknown) => void;
+          const inviteeAuth = await testFixture.signUpAndLoginEmail(
+            "poorguy@email.com"
+          );
+          const token = pipe(split(" "), last)(inviteeAuth);
 
-        return expect(userPromise).resolves.toEqual({
-          exampleData: "FirstTest!",
+          recipientSocket = ioc(connectionAddress, {
+            auth: {
+              token,
+            },
+          });
+
+          const userReceivesMessagesPromise = new Promise((resolve) => {
+            resolveUserReceivesMessagesPromise = resolve;
+          });
+
+          recipientSocket.connect();
+          const messages = [];
+          let messagesReceived = 0;
+          recipientSocket.on("example_event", (details) => {
+            messagesReceived++;
+            messages.push(details);
+            if (messagesReceived == 2) {
+              resolveUserReceivesMessagesPromise(messages);
+            }
+          });
+
+          await singleUserPromise;
+          const dispatchNotification = createDispatchNotification(server);
+
+          dispatchNotification({
+            recipient: "poorguy@email.com",
+            type: "example_event",
+            payload: {
+              exampleData: "FirstNotification!",
+            },
+          });
+
+          dispatchNotification({
+            recipient: "poorguy@email.com",
+            type: "example_event",
+            payload: {
+              exampleData: "SecondNotification!",
+            },
+          });
+
+          await expect(userReceivesMessagesPromise).resolves.toEqual([
+            {
+              exampleData: "FirstNotification!",
+            },
+            {
+              exampleData: "SecondNotification!",
+            },
+          ]);
         });
       });
     });
-    describe(`when a message is dispatched to another user`, () => {
-      let inviteeSocket: Socket;
-      let thirdPartySocket: Socket;
+    describe(`and another user is connected`, () => {
+      describe(`when a message is dispatched to another user`, () => {
+        let inviteeSocket: Socket;
+        let thirdPartySocket: Socket;
 
-      afterEach(() => {
-        thirdPartySocket.removeAllListeners();
-        thirdPartySocket.disconnect();
-        inviteeSocket.removeAllListeners();
-        inviteeSocket.disconnect();
+        afterEach(() => {
+          thirdPartySocket.removeAllListeners();
+          thirdPartySocket.disconnect();
+          inviteeSocket.removeAllListeners();
+          inviteeSocket.disconnect();
+        });
+        it("only sends the message to the intended recipient", async () => {
+          const firstUserConnectionPromise = new Promise((resolve) => {
+            resolvePromiseWhenUserJoinsRoom = resolve;
+          });
+
+          let resolveInviteeEventPromise: (value: unknown) => void;
+
+          const promiseToResolveWhenInviteeReceivesEvent = new Promise(
+            (resolve) => {
+              resolveInviteeEventPromise = resolve;
+            }
+          );
+          const inviteeAuth = await testFixture.signUpAndLoginEmail(
+            "invitee@email.com"
+          );
+          const thirdPartyAuth = await testFixture.signUpAndLoginEmail(
+            "thirdParty@email.com"
+          );
+          const thirdPartyToken = pipe(split(" "), last)(thirdPartyAuth);
+          const inviteeToken = pipe(split(" "), last)(inviteeAuth);
+
+          thirdPartySocket = ioc(connectionAddress, {
+            auth: {
+              token: thirdPartyToken,
+            },
+          });
+
+          await firstUserConnectionPromise;
+
+          inviteeSocket = ioc(connectionAddress, {
+            auth: {
+              token: inviteeToken,
+            },
+          });
+
+          thirdPartySocket.on("example_event", (details) => {
+            expect(true).toBeFalsy();
+          });
+
+          const secondUserConnectionPromise = new Promise((resolve) => {
+            resolvePromiseWhenUserJoinsRoom = resolve;
+          });
+
+          inviteeSocket.on("example_event", (details) => {
+            resolveInviteeEventPromise(details);
+          });
+          await secondUserConnectionPromise;
+
+          const dispatchNotification = createDispatchNotification(server);
+          dispatchNotification({
+            recipient: "invitee@email.com",
+            type: "example_event",
+            payload: {
+              exampleData: "SecondTest!",
+            },
+          });
+
+          await expect(
+            promiseToResolveWhenInviteeReceivesEvent
+          ).resolves.toEqual({
+            exampleData: "SecondTest!",
+          });
+        });
       });
-      it("only sends the message to the intended recipient", async () => {
-        const firstUserConnectionPromise = new Promise((resolve) => {
-          resolvePromiseWhenUserJoinsRoom = resolve;
-        });
+      describe(`when a message is dispatched to each user`, () => {
+        it.skip(`each user receives a message`, async () => {
+          const firstUserConnectionPromise = new Promise((resolve) => {
+            resolvePromiseWhenUserJoinsRoom = resolve;
+          });
 
-        let resolveInviteeEventPromise: (value: unknown) => void;
+          let resolveInviteeEventPromise: (value: unknown) => void;
 
-        const promiseToResolveWhenInviteeReceivesEvent = new Promise(
-          (resolve) => {
-            resolveInviteeEventPromise = resolve;
-          }
-        );
-        const inviteeAuth = await testFixture.signUpAndLoginEmail(
-          "invitee@email.com"
-        );
-        const thirdPartyAuth = await testFixture.signUpAndLoginEmail(
-          "thirdParty@email.com"
-        );
-        const thirdPartyToken = pipe(split(" "), last)(thirdPartyAuth);
-        const inviteeToken = pipe(split(" "), last)(inviteeAuth);
+          const promiseToResolveWhenInviteeReceivesEvent = new Promise(
+            (resolve) => {
+              resolveInviteeEventPromise = resolve;
+            }
+          );
+          const inviteeAuth = await testFixture.signUpAndLoginEmail(
+            "invitee@email.com"
+          );
+          const thirdPartyAuth = await testFixture.signUpAndLoginEmail(
+            "thirdParty@email.com"
+          );
+          const thirdPartyToken = pipe(split(" "), last)(thirdPartyAuth);
+          const inviteeToken = pipe(split(" "), last)(inviteeAuth);
 
-        thirdPartySocket = ioc(connectionAddress, {
-          auth: {
-            token: thirdPartyToken,
-          },
-        });
+          thirdPartySocket = ioc(connectionAddress, {
+            auth: {
+              token: thirdPartyToken,
+            },
+          });
 
-        await firstUserConnectionPromise;
+          await firstUserConnectionPromise;
 
-        inviteeSocket = ioc(connectionAddress, {
-          auth: {
-            token: inviteeToken,
-          },
-        });
+          inviteeSocket = ioc(connectionAddress, {
+            auth: {
+              token: inviteeToken,
+            },
+          });
 
-        thirdPartySocket.connect();
-        thirdPartySocket.on("example_event", (details) => {
-          expect(true).toBeFalsy();
-        });
+          thirdPartySocket.on("example_event", (details) => {
+            expect(true).toBeFalsy();
+          });
 
-        const secondUserConnectionPromise = new Promise((resolve) => {
-          resolvePromiseWhenUserJoinsRoom = resolve;
-        });
-        inviteeSocket.connect();
-        inviteeSocket.on("example_event", (details) => {
-          resolveInviteeEventPromise(details);
-        });
-        await secondUserConnectionPromise;
+          const secondUserConnectionPromise = new Promise((resolve) => {
+            resolvePromiseWhenUserJoinsRoom = resolve;
+          });
 
-        const dispatchNotification = createDispatchNotification(server);
-        dispatchNotification({
-          recipient: "invitee@email.com",
-          type: "example_event",
-          payload: {
+          inviteeSocket.on("example_event", (details) => {
+            resolveInviteeEventPromise(details);
+          });
+          await secondUserConnectionPromise;
+
+          const dispatchNotification = createDispatchNotification(server);
+          dispatchNotification({
+            recipient: "invitee@email.com",
+            type: "example_event",
+            payload: {
+              exampleData: "SecondTest!",
+            },
+          });
+
+          await expect(
+            promiseToResolveWhenInviteeReceivesEvent
+          ).resolves.toEqual({
             exampleData: "SecondTest!",
-          },
+          });
         });
-        await expect(promiseToResolveWhenInviteeReceivesEvent).resolves.toEqual(
-          {
-            exampleData: "SecondTest!",
-          }
-        );
-        Promise.resolve(firstUserConnectionPromise);
-        Promise.resolve(secondUserConnectionPromise);
       });
     });
   });
