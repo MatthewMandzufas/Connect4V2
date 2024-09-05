df122c991cddb5aa799e666dec723b907037cd7a
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-09-05 16:19:23 +0800

    RED: in-memory-game-repository › loading a game › given a game has been saved › when given the uuid › returns the game

--- src/game/in-memory-game-repository.spec.ts
+++ src/game/in-memory-game-repository.spec.ts
@@ -3,6 +3,25 @@ import InMemoryGameRepository from "./in-memory-game-repository";
 
 describe("in-memory-game-repository", () => {
   let gameRepository: InMemoryGameRepository;
+  const gameDetails = {
+    board: [[{ occupyingPlayer: 1 }]],
+    activePlayer: 1,
+    players: {
+      1: {
+        player: 1,
+        discsLeft: 1,
+      },
+      2: {
+        player: 2,
+        discsLeft: 2,
+      },
+    },
+    status: GameStatus.IN_PROGRESS,
+    playerColors: {
+      playerOneColor: "red",
+      playerTwoColor: "yellow",
+    },
+  } satisfies GameDetails;
   beforeEach(() => {
     gameRepository = new InMemoryGameRepository();
   });
@@ -13,28 +32,9 @@ describe("in-memory-game-repository", () => {
     });
   });
   describe("saving a game", () => {
-    describe("given a gam to save", () => {
+    describe("given a game to save", () => {
       it("saves the game", () => {
-        const game = {
-          board: [[{ occupyingPlayer: 1 }]],
-          activePlayer: 1,
-          players: {
-            1: {
-              player: 1,
-              discsLeft: 1,
-            },
-            2: {
-              player: 2,
-              discsLeft: 2,
-            },
-          },
-          status: GameStatus.IN_PROGRESS,
-          playerColors: {
-            playerOneColor: "red",
-            playerTwoColor: "yellow",
-          },
-        } satisfies GameDetails;
-        expect(gameRepository.saveGame(game)).resolves.toEqual(
+        expect(gameRepository.saveGame(gameDetails)).resolves.toEqual(
           expect.objectContaining({
             uuid: expect.toBeUUID(),
             board: [[{ occupyingPlayer: 1 }]],
@@ -55,6 +55,17 @@ describe("in-memory-game-repository", () => {
             },
           })
         );
+      });
+    });
+  });
+  describe("loading a game", () => {
+    describe("given a game has been saved", () => {
+      describe("when given the uuid", () => {
+        it("returns the game", async () => {
+          const { uuid } = await gameRepository.saveGame(gameDetails);
+
+          expect(gameRepository.loadGame(uuid)).resolves.toEqual({});
+        });
       });
     });
   });
