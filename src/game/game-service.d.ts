23cf6bd38e82ee3fb166da6bd7dd07d61b3d8eb2
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-09-05 16:09:58 +0800

    RED: in-memory-game-repository › saving a game › given a gam to save › saves the game

--- src/game/game-service.d.ts
+++ src/game/game-service.d.ts
@@ -1,1 +1,34 @@
 export interface GameRepository {}
+
+export type GameDetails = {
+  board: Board;
+  activePlayer: PlayerNumber;
+  players: Record<PlayerNumber, PlayerStats>;
+  status: GameStatus;
+  playerColors: PlayerColorsType;
+};
+
+export type Board = Array<Array<BoardCell>>;
+
+export type BoardCell = {
+  occupyingPlayer?: PlayerNumber;
+};
+
+export type PlayerNumber = 1 | 2;
+
+export interface PlayerStats {
+  player: PlayerNumber;
+  discsLeft: number;
+}
+
+export enum GameStatus {
+  IN_PROGRESS = "IN_PROGRESS",
+  PLAYER_ONE_WIN = "PLAYER_ONE_WIN",
+  PLAYER_TWO_WIN = "PLAYER_TWO_WIN",
+  DRAW = "DRAW",
+}
+
+export type PlayerColorsType = {
+  playerOneColor: string;
+  playerTwoColor: string;
+};
--- src/game/in-memory-game-repository.spec.ts
+++ src/game/in-memory-game-repository.spec.ts
@@ -1,11 +1,60 @@
+import { GameStatus } from "./game-service.d";
 import InMemoryGameRepository from "./in-memory-game-repository";
 
 describe("in-memory-game-repository", () => {
+  let gameRepository: InMemoryGameRepository;
+  beforeEach(() => {
+    gameRepository = new InMemoryGameRepository();
+  });
   describe("creating a game repository", () => {
-    describe("given no arguments", () => {
-      it("creates an in memory game repository", () => {
-        const repository = new InMemoryGameRepository();
-        expect(repository).toBeInstanceOf(InMemoryGameRepository);
+    it("creates an in memory game repository", () => {
+      const repository = new InMemoryGameRepository();
+      expect(repository).toBeInstanceOf(InMemoryGameRepository);
+    });
+  });
+  describe("saving a game", () => {
+    describe("given a gam to save", () => {
+      it("saves the game", () => {
+        const game = {
+          board: [[1]],
+          activePlayer: 1,
+          players: {
+            1: {
+              player: 1,
+              discsLeft: 1,
+            },
+            2: {
+              player: 2,
+              discsLeft: 2,
+            },
+          },
+          status: GameStatus.IN_PROGRESS,
+          playerColors: {
+            playerOneColor: "red",
+            playerTwoColor: "yellow",
+          },
+        };
+        expect(gameRepository.saveGame(game)).resolves.toEqual(
+          expect.objectContaining({
+            uuid: expect.toBeUUID(),
+            board: [[{ occupyingPlayer: 1 }]],
+            players: {
+              1: {
+                player: 1,
+                discsLeft: 1,
+              },
+              2: {
+                player: 2,
+                discsLeft: 2,
+              },
+            },
+            status: GameStatus.IN_PROGRESS,
+            playerColors: {
+              playerOneColor: "red",
+              playerTwoColor: "yellow",
+            },
+          })
+        );
       });
     });
   });
