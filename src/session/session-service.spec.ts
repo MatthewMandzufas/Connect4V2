9e83cbf8a85820508e19d4e14e90f7ff02e0d4fe
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-09-10 15:35:28 +0800

    RED: session-service › adding games › given an in-progress session › with no games › adds a new game to the session

--- src/session/session-service.spec.ts
+++ src/session/session-service.spec.ts
@@ -75,4 +75,29 @@ describe("session-service", () => {
       });
     });
   });
+  describe("adding games", () => {
+    describe("given an in-progress session", () => {
+      describe("with no games", () => {
+        it("adds a new game to the session", async () => {
+          const { uuid: sessionUuid } = await sessionService.createSession({
+            inviterUuid: "70b9b52d-b993-4108-8719-8490878a3e35",
+            inviteeUuid: "e5fef403-214c-46de-89be-655b90b9a79f",
+          });
+
+          expect(sessionService.getGameUuids(sessionUuid)).resolves.toEqual([]);
+          expect(
+            sessionService.getActiveGameId(sessionUuid)
+          ).resolves.toBeUndefined();
+          await sessionService.addNewGame();
+          const activeGameId = await sessionService.getActiveGameId(
+            sessionUuid
+          );
+          expect(activeGameId).toBeUUID();
+          expect(sessionService.getGameUuids(sessionUuid)).resolves.toEqual([
+            activeGameId,
+          ]);
+        });
+      });
+    });
+  });
 });
