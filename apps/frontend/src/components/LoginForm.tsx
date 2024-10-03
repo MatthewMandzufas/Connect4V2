b33add627236b8b9231e09d4fc8633cfd5a1eb25
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-10-03 10:19:36 +0800

    Add further loginForm stories for successful/unsuccessful login attempts

--- apps/frontend/src/components/LoginForm.tsx
+++ apps/frontend/src/components/LoginForm.tsx
@@ -1,57 +1,150 @@
 import { useState } from "react";
+import PulseLoader from "react-spinners/PulseLoader";
 
 type LoginDetails = {
   email: string;
   password: string;
 };
 
-type LoginHandler = (loginDetails: LoginDetails) => unknown;
+type LoginResponse = {
+  isSuccess: boolean;
+  message: string;
+};
+
+type LoginHandler = (loginDetails: LoginDetails) => Promise<LoginResponse>;
 
 type LoginFormProps = {
   loginHandler: LoginHandler;
 };
 
+type ValidationResult = {
+  isValid: boolean;
+  error?: string;
+};
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const validateFields = ({
+  email,
+  password,
+}: LoginDetails): ValidationResult => {
+  if (!email && !password) {
+    return {
+      isValid: false,
+      error: "All fields are required.",
+    };
+  }
+
+  if (!email.match(emailRegex)) {
+    return {
+      isValid: false,
+      error: "Invalid email address.",
+    };
+  }
+
+  if (password.length <= 5) {
+    return {
+      isValid: false,
+      error: "Password must be at least 6 letters.",
+    };
+  }
+
+  return {
+    isValid: true,
+  };
+};
+
 const LoginForm = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  loginHandler = () => {},
+  loginHandler = () => Promise.reject(),
 }: LoginFormProps) => {
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
+  const [isLoginPending, setIsLoginPending] = useState<boolean>(false);
+  const [loginErrorMessage, setLoginErrorMessage] = useState<string>();
+  const [loginSuccessMessage, setLoginSuccessMessage] = useState<string>();
 
   return (
     <form className="flex flex-col max-w-44 gap-3">
       <label htmlFor="email">Email</label>
       <input
+        readOnly={isLoginPending || loginSuccessMessage !== undefined}
         className="text-black"
         type="email"
         name="email"
         id="email"
+        placeholder="Enter Your Email"
         required
-        onChange={(event) => setEmail(event.target.value)}
+        onChange={(event) => {
+          setEmail(event.target.value);
+          setLoginErrorMessage(undefined);
+        }}
       />
       <label htmlFor="password">Password</label>
       <input
         className="text-black"
         type="password"
         name="password"
+        placeholder="Enter Your Password"
         id="password"
-        onChange={(event) => setPassword(event.target.value)}
-        required
-      />
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        onClick={(event) => {
-          event.preventDefault();
-          if (email && password) {
-            loginHandler({ email, password });
-          }
+        onChange={(event) => {
+          setPassword(event.target.value);
+          setLoginErrorMessage(undefined);
         }}
-      >
-        Submit
-      </button>
+        required
+        readOnly={isLoginPending || loginSuccessMessage !== undefined}
+      />
+      {isLoginPending ? (
+        <button
+          className="text-white bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:focus:ring-blue-800"
+          disabled
+        >
+          <PulseLoader loading={true} color={"#ffffff"} size={10} />
+        </button>
+      ) : (
+        <button
+          disabled={loginSuccessMessage !== undefined}
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={async (event) => {
+            event.preventDefault();
+
+            if (email && password) {
+              const { isValid, error } = validateFields({ email, password });
+
+              if (!isValid) {
+                setLoginErrorMessage(error);
+                return;
+              }
+
+              setLoginErrorMessage(undefined);
+              setIsLoginPending(true);
+              try {
+                const { message } = await loginHandler({
+                  email,
+                  password,
+                });
+                setLoginSuccessMessage(message);
+              } catch (error) {
+                const { message } = error as LoginResponse;
+                setLoginErrorMessage(message);
+              }
+              setIsLoginPending(false);
+            }
+          }}
+        >
+          Submit
+        </button>
+      )}
+      {loginErrorMessage ? (
+        <p className="text-red-600 underline">{loginErrorMessage}</p>
+      ) : (
+        <></>
+      )}
+      {loginSuccessMessage ? (
+        <p className="text-green-600 underline">{loginSuccessMessage}</p>
+      ) : (
+        <></>
+      )}
     </form>
   );
 };
-
 export default LoginForm;
--- apps/frontend/src/stories/loginForm.stories.ts
+++ apps/frontend/src/stories/loginForm.stories.ts
@@ -1,17 +1,108 @@
 import LoginForm from "@/components/LoginForm";
 import { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
+import { fn, userEvent, within } from "@storybook/test";
 
 type Story = StoryObj<typeof LoginForm>;
 
 const meta: Meta<typeof LoginForm> = {
+  // title: "Testing/1/2",
   component: LoginForm,
 };
 
 export default meta;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const filOutLoginFormCorrectly = async (canvas: any) => {
+  const emailInput = canvas.getByPlaceholderText("Enter Your Email");
+  const passwordInput = canvas.getByPlaceholderText("Enter Your Password");
+  await userEvent.type(emailInput, "hello@email.com");
+  await userEvent.type(passwordInput, "somethingSecure");
+};
+
+// Stories before login button is clicked
 export const TheOneWithDefaults: Story = {};
 export const TheOneWithALoginHandler: Story = {
   args: {
-    loginHandler: fn(),
+    loginHandler: fn((loginValues) => {
+      return loginValues;
+    }),
+  },
+};
+
+export const TheOneWithAnInvalidEmail: Story = {
+  args: {
+    loginHandler: fn((loginValues) => {
+      return loginValues;
+    }),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const emailInput = canvas.getByPlaceholderText("Enter Your Email");
+    const passwordInput = canvas.getByPlaceholderText("Enter Your Password");
+    await userEvent.type(emailInput, "faultyEmail");
+    await userEvent.type(passwordInput, "somethingSecure");
+    const loginButton = canvas.getByRole("button");
+    await userEvent.click(loginButton);
+  },
+};
+
+export const TheOneWithAnInvalidPassword: Story = {
+  args: {
+    loginHandler: fn((loginValues) => {
+      return loginValues;
+    }),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const emailInput = canvas.getByPlaceholderText("Enter Your Email");
+    const passwordInput = canvas.getByPlaceholderText("Enter Your Password");
+    await userEvent.type(emailInput, "hello@email.com");
+    await userEvent.type(passwordInput, "short");
+    const loginButton = canvas.getByRole("button");
+    await userEvent.click(loginButton);
+  },
+};
+
+// Stories with a pending login
+export const TheOneWithALoginInProgress: Story = {
+  args: {
+    loginHandler: fn(() => Promise.race([])),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await filOutLoginFormCorrectly(canvas);
+    const loginButton = canvas.getByRole("button");
+    await userEvent.click(loginButton);
+  },
+};
+
+// Stories with a failed login
+
+export const TheOneWithAFailedLogin: Story = {
+  args: {
+    loginHandler: fn(() =>
+      Promise.reject({ isSuccess: false, message: "Login Failed" })
+    ),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await filOutLoginFormCorrectly(canvas);
+    const loginButton = canvas.getByRole("button");
+    await userEvent.click(loginButton);
+  },
+};
+
+export const TheOneWithASuccessfulLogin: Story = {
+  args: {
+    loginHandler: fn(() =>
+      Promise.resolve({ isSuccess: true, message: "Login Successful!" })
+    ),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await filOutLoginFormCorrectly(canvas);
+    const loginButton = canvas.getByRole("button");
+    await userEvent.click(loginButton);
   },
 };
