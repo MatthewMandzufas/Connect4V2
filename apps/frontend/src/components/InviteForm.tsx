538242fae253e83112a92eb3a7d254326685f3c8
dempseyt <dempsey.thompson@journeyone.com.au>
2024-11-20 09:52:24 +0800

    Add stories for the Invite Form

--- apps/frontend/src/components/InviteForm.tsx
+++ apps/frontend/src/components/InviteForm.tsx
@@ -1,15 +1,45 @@
-const InviteForm = () => {
+import { useState } from "react";
+
+type InviteValues = {
+  invitee: string;
+};
+
+type InviteResponse = {
+  isSuccess: boolean;
+  message: string;
+};
+
+type InviteFormProps = {
+  inviteHandler: (inviteValues: InviteValues) => Promise<InviteResponse>;
+};
+
+const InviteForm = ({ inviteHandler }: InviteFormProps) => {
+  const [invitee, setInvitee] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [message, setMessage] = useState("");
+
   return (
-    <div className="flex flex-col justify-center gap-5 items-center min-h-10 w-40 border border-black py-10 ">
+    <div className="flex flex-col justify-start gap-5 items-center min-h-10 max-h- max-w-48 border border-black py-10 ">
       <input
         type="text"
-        className="border border-solid border-black rounded max-w-28 text-xs"
+        className="border border-solid border-black rounded w-3/4 text-xs p-2"
+        onChange={(event) => {
+          setInvitee(event.target.value);
+        }}
         placeholder="Enter email here..."
       />
-      <button className="border border-solid rounded border-black p-1 text-blue-500 hover:text-blue-900 ">
+      <button
+        onClick={async (event) => {
+          event.preventDefault();
+          const response = await inviteHandler({ invitee });
+          setIsSuccess(response.isSuccess);
+          setMessage(response.message);
+        }}
+        className="border border-solid rounded border-black p-1 text-blue-500 hover:text-blue-900 "
+      >
         Send
       </button>
-      <h1></h1>
+      <p className={isSuccess ? "text-green-500" : "text-red-500"}>{message}</p>
     </div>
   );
 };
--- apps/frontend/src/stories/InviteForm.stories.ts
+++ apps/frontend/src/stories/InviteForm.stories.ts
@@ -1,5 +1,6 @@
 import InviteForm from "@/components/InviteForm";
 import { Meta, StoryObj } from "@storybook/react";
+import { fn, userEvent, within } from "@storybook/test";
 
 type Story = StoryObj<typeof InviteForm>;
 
@@ -10,3 +11,40 @@ const meta: Meta<typeof InviteForm> = {
 export default meta;
 
 export const TheOneWithDefaults: Story = {};
+export const TheOneWithAInviteHandler: Story = {
+  args: {
+    inviteHandler: fn((inviteValues) => {
+      return inviteValues;
+    }),
+  },
+};
+
+export const TheOneWithAInvalidEmail: Story = {
+  args: {
+    inviteHandler: fn(() =>
+      Promise.resolve({ isSuccess: false, message: "User does not exist" })
+    ),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const inviteeInput = canvas.getByPlaceholderText("Enter email here...");
+    await userEvent.type(inviteeInput, "john@mail.com");
+    const sendInviteButton = canvas.getByRole("button");
+    await userEvent.click(sendInviteButton);
+  },
+};
+
+export const TheOneWithAValidEmail: Story = {
+  args: {
+    inviteHandler: fn(() =>
+      Promise.resolve({ isSuccess: true, message: "Invite sent" })
+    ),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const inviteeInput = canvas.getByPlaceholderText("Enter email here...");
+    await userEvent.type(inviteeInput, "pork@mail.com");
+    const sendInviteButton = canvas.getByRole("button");
+    await userEvent.click(sendInviteButton);
+  },
+};
